e8a12e0ba81b7437cca399df53ddf998d8c9f201
Yuan Junjie <yjj@uniteman.com>
2022-12-09 15:02:54 +0800

    fix dialog context invalid show

--- common/src/main/java/com/jun/template/common/base/BaseDialog.kt
+++ common/src/main/java/com/jun/template/common/base/BaseDialog.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.os.Bundle
 import androidx.viewbinding.ViewBinding
 import com.jun.template.common.R
+import com.jun.template.common.utils.ContextUtils
 
 /**
  *
@@ -12,8 +13,8 @@ import com.jun.template.common.R
  * @author Jun
  * @time 2022/6/29
  */
-abstract class BaseDialog<VB : ViewBinding>(context: Context) :
-    Dialog(context, R.style.RoundDialogTheme) {
+abstract class BaseDialog<VB : ViewBinding>(private val mContext: Context) :
+    Dialog(mContext, R.style.RoundDialogTheme) {
 
     lateinit var vb: VB
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +24,11 @@ abstract class BaseDialog<VB : ViewBinding>(context: Context) :
         init()
     }
 
+    override fun show() {
+        if (ContextUtils.isValidContext(mContext)) {
+            super.show()
+        }
+    }
 
     abstract fun getViewBinding(): VB
     abstract fun init()
